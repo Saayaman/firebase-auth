ebb08a1aa6f8d99b003f0fb2192b672ba106623c
Ayako <ayakosayama1987@gmail.com>
2019-08-21 10:20:47 -0700

    update

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -3,17 +3,52 @@ import React, { Component } from 'react'
 export default class SignUp extends Component {
   state = {
     username: '',
+    email: '',
+    passwordOne: '',
+    passwordTwo: '',
+    error: '',
   }
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    })
+  }
+
   render() {
-    const { username } = this.state;
+    const { username, email, passwordOne, passwordTwo, error } = this.state;
     return (
       <form>
         <input
           name="username"
           value={username}
+          onChange={this.handleChange}
           type="text"
-          placeholder=""
+          placeholder="Full name"
         />
+        <input
+          name="email"
+          value={email}
+          onChange={this.handleChange}
+          type="email"
+          placeholder="email"
+        />
+        <input
+          name="passwordOne"
+          value={passwordOne}
+          onChange={this.handleChange}
+          type="password"
+          placeholder="password"
+        />
+        <input
+          name="passwordTwo"
+          value={passwordTwo}
+          onChange={this.handleChange}
+          type="password"
+          placeholder="confirmpassword"
+        />
+        <button type="submit">Sign Up</button>
+        {error && <p>{error.message}</p>}
       </form>
     )
   }
